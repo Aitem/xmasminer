e38c258e106128a092f79d079d56975b31e64af6
panthevm <panthevm@yandex.ru>
2022-12-27 21:52:58 +0100

    Custom key listener

---
+++ src/app/init.cljs
@@ -0,0 +1,13 @@
+(ns app.init
+  (:require [re-frame.core :as rf]))
+
+(defonce initialize
+  (js/document.addEventListener
+   "keydown"
+   (fn [event]
+     (case (.-key event)
+       "w" (rf/dispatch-sync [:app.player/move-w])
+       "a" (rf/dispatch-sync [:app.player/move-a])
+       "s" (rf/dispatch-sync [:app.player/move-s])
+       "d" (rf/dispatch-sync [:app.player/move-d])
+       nil))))
